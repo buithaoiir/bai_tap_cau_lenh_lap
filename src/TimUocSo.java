0714dea2bfdd9cd36c24c39fd013ac0b91438bf9
thaobui <buithaoiir@gmail.com>
2018-08-01 10:52:57 +0700

    khai bao bien, hien thi lenh nhap so

---
+++ src/TimUocSo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TimUocSo {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        a = nhap.nextInt();
+        System.out.println("Nhap b: ");
+        b = nhap.nextInt();
+
+    }
+
+}
