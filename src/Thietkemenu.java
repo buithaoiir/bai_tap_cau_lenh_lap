845e393798c0005ec25ff90f86db76d242b111b3
thaobui <buithaoiir@gmail.com>
2018-08-01 09:30:23 +0700

    hien thi lua chon tu input do nguoi dung nhap

--- src/Thietkemenu.java
+++ src/Thietkemenu.java
@@ -11,6 +11,36 @@ public class Thietkemenu {
     System.out.println("0. Exit");
     System.out.println("Enter your choice: ");
     choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+                break;
+            case 2:
+                System.out.println("Draw the square");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 3:
+                System.out.println("Draw the rectangle");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.print("No choice!");
+        }
 
 }
 }
