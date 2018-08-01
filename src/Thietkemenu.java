85283c1d4c0f693d99b879922c6db0388b713d01
thaobui <buithaoiir@gmail.com>
2018-08-01 09:16:15 +0700

    khai bao bien, tao menu lua chon

---
+++ src/Thietkemenu.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Thietkemenu {
+    public static void main(String[] args) {
+    int choice;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Menu");
+    System.out.println("1. Draw the triangle");
+    System.out.println("3. Draw the square");
+    System.out.println("0. Exit");
+    System.out.println("Enter your choice: ");
+    choice = input.nextInt();
+
+}
+}
+
+
